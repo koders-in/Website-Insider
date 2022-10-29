0318c97e7ff64aff30075f1b05cbc272c43fa82f
Saksham <sakshamchauhan79@gmail.com>
2022-10-29 13:10:43 +0530

    fixed the issue of redirect to home page from slug page

--- helper/constant.ts
+++ helper/constant.ts
@@ -45,7 +45,7 @@ export const footerButtons = [
   {
     title: "Quick Links",
     buttons: [
-      { title: "Blogs", route: "Blogs", target: false },
+      { title: "Blogs", route: "/Blogs", target: false },
       {
         title: "Portfolio",
         route: "https://portfolio.koders.in/",
@@ -58,10 +58,10 @@ export const footerButtons = [
   {
     title: "Legal",
     buttons: [
-      { title: "Privacy Policy", route: "PrivacyPolicy", target: false },
-      { title: "Terms of Services", route: "TermsOfServic", target: false },
-      { title: "Cancellation", route: "Cancellation", target: false },
-      { title: "Refund", route: "Cancellation", target: false },
+      { title: "Privacy Policy", route: "/PrivacyPolicy", target: false },
+      { title: "Terms of Services", route: "/TermsOfServic", target: false },
+      { title: "Cancellation", route: "/Cancellation", target: false },
+      { title: "Refund", route: "/Cancellation", target: false },
     ],
   },
   {
