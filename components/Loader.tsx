7c13b727128544d061b1b390cbbe78e393a7635f
Saksham <sakshamchauhan79@gmail.com>
2022-11-08 19:01:06 +0530

    changed testmonial loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { FadeLoader } from "react-spinners";
 
 const Loader = () => {
   return (
     <div className="absolute w-full h-full backdrop-blur-md z-50 flex justify-center items-center">
-      <div className="border-2 border-white border-b-transparent rounded-full w-[90px] h-[90px] animate-spin"></div>
+      <FadeLoader
+        color="#00A99D"
+        loading={true}
+        // cssOverride={override}
+        size={100}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
     </div>
   );
 };
